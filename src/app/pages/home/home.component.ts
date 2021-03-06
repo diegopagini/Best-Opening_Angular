601a8b2a07ab75075a86d946bc2a32366bf5d811
Diego Pagini <dq@qbkconsulting.com>
2021-03-06 14:34:01 -0300

    videos cargados

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -10,7 +10,46 @@ export class HomeComponent implements OnInit {
   openings = [
     { name: 'Pokemon', video: 'https://www.youtube.com/embed/uDIoEbbFKAY' },
     { name: 'Dragon Ball', video: 'https://www.youtube.com/embed/9Hbd1QeI1Og' },
-    { name: 'Naruto', video: 'https://www.youtube.com/embed/uMyuSHewmks' },
+    {
+      name: 'Ranma 1/2',
+      video: 'https://www.youtube.com/embed/OcYKuG9Ceys',
+    },
+    {
+      name: 'Sailor Moon',
+      video: 'https://www.youtube.com/embed/Zq8_XppDXdk',
+    },
+    {
+      name: 'Inuyasha',
+      video: 'https://www.youtube.com/embed/MwyQf8BaOK4',
+    },
+    {
+      name: 'Caballeros del Zodiaco',
+      video: 'https://www.youtube.com/embed/XkUV1Ry9q7Q',
+    },
+    {
+      name: 'Slam Dunk',
+      video: 'https://www.youtube.com/embed/euW71JOlKy0',
+    },
+    {
+      name: 'Dragon Ball Z',
+      video: 'https://www.youtube.com/embed/-r27tgxNr1I',
+    },
+    {
+      name: 'Bt X',
+      video: 'https://www.youtube.com/embed/rxcgCwjYVdM',
+    },
+    {
+      name: 'El Baron Rojo',
+      video: 'https://www.youtube.com/embed/dQPiE0qN_gE&t=72s',
+    },
+    {
+      name: 'Dragon Ball GT',
+      video: 'https://www.youtube.com/embed/wgZ-ATUGXDw',
+    },
+    {
+      name: 'Yu-Gi-Oh!',
+      video: 'https://www.youtube.com/embed/fZZkvMKd4Fs',
+    },
   ];
 
   backgroundImgs = [
